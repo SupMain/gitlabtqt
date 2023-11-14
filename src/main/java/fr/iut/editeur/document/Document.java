2f28050c57561e4d84c033a96ccd2e78cd0b6047
SupMain <90897562+SupMain@users.noreply.github.com>
2023-11-14 10:21:07 +0100

    Fini TP1

--- src/main/java/fr/iut/editeur/document/Document.java
+++ src/main/java/fr/iut/editeur/document/Document.java
@@ -2,65 +2,65 @@ package fr.iut.editeur.document;
 
 public class Document {
 
-    private String texte;
+    private String contentDocument;
 
     public Document() {
-        this.texte = "";
+        this.contentDocument = "";
     }
 
 
 	
     public String getTexte() {
-        return texte;
+        return contentDocument;
     }
 
-    public void setTexte(String texte) {
-        this.texte = texte;
+    public void setTexte(String contentDocument) {
+        this.contentDocument = contentDocument;
     }
 
     public void ajouter(String texte) {
-        this.texte += texte;
+        this.contentDocument += texte;
     }
 
     public void remplacer(int start, int end, String remplacement) {
         if (remplacement.isEmpty()) {
             remplacement = " ";
         }
-        String leftPart = texte.substring(0, start);
-        String rightPart = texte.substring(end);
-        texte = leftPart + remplacement + rightPart;
+        String leftPart = contentDocument.substring(0, start);
+        String rightPart = contentDocument.substring(end);
+        contentDocument = leftPart + remplacement + rightPart;
     }
 
     public void majuscules(int start, int end) {
-        String portionEnMajuscules = texte.substring(start, end).toUpperCase();
+        String portionEnMajuscules = contentDocument.substring(start, end).toUpperCase();
         remplacer(start, end, portionEnMajuscules);
     }
     public void minuscules(int start, int end){
-        String portionEnMinuscule = texte.substring(start, end).toLowerCase();
+        String portionEnMinuscule = contentDocument.substring(start, end).toLowerCase();
         remplacer(start, end, portionEnMinuscule);
     }
 
     public void effacer(int start, int end) {
-        String texteAvant = texte.substring(0, start);
-        String texteApres = texte.substring(end);
-        texte = texteAvant + texteApres;
+        String texteAvant = contentDocument.substring(0, start);
+        String texteApres = contentDocument.substring(end);
+        contentDocument = texteAvant + texteApres;
     }
 
     public void clear() {
-        remplacer(0, this.texte.length(), "");
+        remplacer(0, this.contentDocument.length(), "");
     }
 
     public void inserer(int index, String chainChara) {
-        if (index < 0 || index > texte.length()) {
+        if (index < 0 || index > contentDocument.length()) {
             throw new IllegalArgumentException("L'indice doit être compris entre 0 et la longueur du texte inclus.");
         }
 
-        String texteAvant = texte.substring(0, index);
-        String texteApres = texte.substring(index);
-        texte = texteAvant + chainChara + texteApres;
+        String texteAvant = contentDocument.substring(0, index);
+        String texteApres = contentDocument.substring(index);
+        contentDocument = texteAvant + chainChara + texteApres;
     }
     @Override
     public String toString() {
-        return this.texte;
+        return this.contentDocument;
     }
 }
